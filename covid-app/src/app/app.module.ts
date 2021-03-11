02e0992ddc3935b078b1dc36dac870a7e0d94b79
Aaron Kirschner <aaron.kirschner@gmail.com>
2021-03-11 11:29:59 -0500

    Added components Changed to scss

--- covid-app/src/app/app.module.ts
+++ covid-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostDetailComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule
